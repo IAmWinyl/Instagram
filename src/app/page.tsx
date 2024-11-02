a59110a8748493e30ce121806b731eab0aa70629
Serra Abak <serra.abak@gmail.com>
2024-11-02 14:59:04 -0400

    Add signup page link to signin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import SignInForm from "@/components/SignInForm";
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col grow justify-center mt-[12px] max-w-[350px]">
-        <div className="flex flex-col shrink-0 bg-primary_background border-[1px] items-center border-elevated_separator box-border mb-[10px] py-[10px] relative text-primary_text">
+      <div className="flex flex-col grow shrink-0 justify-center mt-[12px] max-w-[350px]">
+        <div className="flex flex-col bg-primary_background border-[1px] border-elevated_separator items-center box-border mb-[10px] py-[10px] relative text-primary_text">
           <button
             type="button"
             className="inline-block bg-[url('https://static.cdninstagram.com/rsrc.php/v3/ym/r/BQdTmxpRI6f.png')] bg-cover bg-no-repeat w-[175px] h-[51px] mt-[36px] mb-[12px]"
@@ -12,6 +12,20 @@ export default function Home() {
           <div className="flex flex-col max-w-[350px] mb-[20px] w-full">
             <SignInForm />
           </div>
+        </div>
+        <div className="flex flex-col bg-primary_background border-[1px] border-elevated_separator items-center box-border py-[10px] mb-[10px]">
+          <p className="text-sm m-[15px]">
+            Don't have an account?&nbsp;
+            <a
+              href="/accounts/emailsignup/"
+              role="link"
+              className="touch-manipulation"
+            >
+              <span className="text-primary_button inline font-semibold">
+                Sign up
+              </span>
+            </a>
+          </p>
         </div>
       </div>
     </div>
