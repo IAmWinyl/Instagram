a859617441fccbfde2b847a204f27cb295ea2b33
Serra Abak <serra.abak@gmail.com>
2024-11-12 08:28:08 -0500

    Steamline Image Carousel timing

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 
 const ImageCarousel = () => {
@@ -10,43 +12,47 @@ const ImageCarousel = () => {
   ];
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setActiveImageId((prevId) => {
-        if (prevId === 0) return 2;
-        if (prevId === images.length) return 1; // Loop back to the first image after the last
-        return prevId + 1;
-      });
-    }, 5000); // Switch images every 5 seconds
+    const timeout = setTimeout(() => {
+      setActiveImageId(2); // Skip to 2nd image after initial load
 
-    return () => clearInterval(interval); // Clean up on component unmount
+      const interval = setInterval(() => {
+        setActiveImageId((prevId) => {
+          if (prevId === images.length) return 1; // Loop back to the 1st after last
+          return prevId + 1;
+        });
+      }, 5000); // Switch images every 5 seconds
+
+      // return () => clearInterval(interval); // Clean up on component unmount
+    }, 3000);
+
+    return () => clearTimeout(timeout); // Clean up on component unmount
   }, []);
 
   const getTransitionStyle = (id: number) => {
-    if (activeImageId === 0 && id === 1) {
-      return "absolute visible"; // First image visible on initial load
-    }
     if (activeImageId === id) {
-      return "absolute animate-fadeIn"; // Active image fades in
+      return "animate-fadeIn"; // Active image fades in
     }
-    // When cycling back, set the last image to fade out
-    // For other cases, the previous image is just activeImageId - 1
     if (
       (activeImageId === 1 && id === images.length) ||
       activeImageId - 1 === id
     ) {
-      return "absolute animate-fadeOut"; // Previous image fades out
+      return "animate-fadeOut"; // Previous image fades out
     }
-    return "absolute invisible"; // All other images are invisible
+    return "invisible"; // All other images are invisible
   };
 
   return (
     <>
-      {images.map((src, index) => (
+      {images.map((src, i) => (
         <img
-          key={index + 1}
+          key={i + 1}
           src={src}
-          className={getTransitionStyle(index + 1)}
-          alt={`Screenshot ${index + 1}`}
+          className={
+            activeImageId === 0
+              ? `absolute ${i === 0 ? "visible" : "invisible"}`
+              : `absolute ${getTransitionStyle(i + 1)}`
+          }
+          alt={`Screenshot ${i + 1}`}
         />
       ))}
     </>
