1113c20ab7e4fc1281df16caffce97a68bb3546c
Serra Abak <serra.abak@gmail.com>
2024-11-12 08:28:41 -0500

    Remove unnecessary state in Input Field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from "react";
 import type { ComponentProps } from "react";
 
@@ -9,17 +11,7 @@ declare type InputFieldProps = {
 };
 
 const InputField = ({ name, field, setField, label }: InputFieldProps) => {
-  const [isValued, setIsValued] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setField(e.target.value);
-    if (e.target.value !== "" && isValued === false) {
-      setIsValued(true);
-    } else if (e.target.value === "" && isValued === true) {
-      setIsValued(false);
-    }
-  };
 
   // Conditional props for `input` tag based on type
   const inputProps: ComponentProps<"input"> = {
@@ -30,42 +22,39 @@ const InputField = ({ name, field, setField, label }: InputFieldProps) => {
     autoCorrect: "off",
     value: field,
     name: name,
-    onChange: handleChange,
+    id: name,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
+      setField(e.target.value),
     ...(name === "username" ? { maxLength: 75 } : {}), // Conditionally apply maxLength only for username
   };
 
   return (
     <div className="mx-[40px] mb-[6px]">
       <div className="flex flex-row bg-secondary_background box-border border-[1px] border-stroke rounded-[3px] text-primary_text text-sm items-center w-full focus-within:border-[1px] focus-within:border-focus_stroke">
-        <label className="flex h-[36px] grow relative">
+        <label htmlFor={name} className="flex h-[36px] grow relative">
           <span
-            className={
-              "font-sans text-secondary_text text-xs absolute left-2 truncate pointer-events-none leading-9 transform origin-top-left ease-out duration-100" +
-              " " +
-              (isValued ? "transform scale-[0.8] translate-y-[-5px]" : "")
-            }
+            className={`font-sans text-secondary_text text-xs absolute left-2 truncate pointer-events-none leading-9 transform origin-top-left ease-out duration-100 ${
+              field !== "" ? "transform scale-[0.8] translate-y-[-5px]" : ""
+            }`}
           >
             {`${label}`}
           </span>
           <input
-            className={
-              "text-left bg-secondary_background grow text-ellipsis overflow-hidden outline-none" +
-              " " +
-              (isValued
-                ? "text-xs pt-[14px] pr-[0px] pb-[2px] pl-[8px]"
-                : "text-base pt-[9px] pr-[0px] pb-[7px] pl-[8px]")
-            }
+            className={`text-left bg-secondary_background grow text-ellipsis overflow-hidden outline-none 
+              ${
+                field !== ""
+                  ? "text-xs pt-[14px] pr-[0px] pb-[2px] pl-[8px]"
+                  : "text-base pt-[9px] pr-[0px] pb-[7px] pl-[8px]"
+              }`}
             {...inputProps}
           />
         </label>
-        {name === "password" && (
-          <div
-            className={"items-center px-2" + " " + (isValued ? "" : "hidden")}
-          >
+        {name === "password" && field !== "" && (
+          <div className={`items-center px-2`}>
             <button
               className="text-sm font-semibold font-sans text-secondary_button hover:opacity-50"
               type="button"
-              onClick={() => setIsVisible(!isVisible)}
+              onClick={() => setIsVisible((prev) => !prev)}
             >
               {isVisible ? "Hide" : "Show"}
             </button>
