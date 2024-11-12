e5c9e9eb05fe9deaac28bdfc36568889911022a7
Serra Abak <serra.abak@gmail.com>
2024-11-12 08:29:49 -0500

    Add Footer and improve spacing in Sign In page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,78 +1,80 @@
-"use client";
-
+import Footer from "@/components/Footer";
 import ImageCarousel from "@/components/ImageCarousel";
 import SignInForm from "@/components/SignInForm";
 
 export default function Home() {
   return (
-    <main
-      className="flex flex-col bg-primary_background justify-center min-w-[100vh]"
-      role="main"
-    >
-      <article className="flex flex-row grow shrink-0 max-w-[--polaris-site-width-wide] w-full mt-[32px] justify-center mx-auto">
-        <div className="pt-[27px] h-[581.15px] mb-3 bg-[length:468.32px_634.15px] bg-[-46px_0px] basis-[380.32px] mr-8 self-center bg-[url(https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones-2x.png?__makehaste_cache_breaker=73SVAexZgBW)]">
-          <div className="relative h-[554.15px] w-[250px] ml-[112px]">
-            <ImageCarousel />
-          </div>
-        </div>
-        <div className="flex flex-col grow shrink-0 justify-center mt-[12px] max-w-[350px]">
-          <div className="flex flex-col bg-primary_background border-[1px] border-elevated_separator items-center box-border mb-[10px] py-[10px] relative text-primary_text">
-            <button
-              type="button"
-              className="inline-block bg-[url('https://static.cdninstagram.com/rsrc.php/v3/ym/r/BQdTmxpRI6f.png')] bg-cover bg-no-repeat w-[175px] h-[51px] mt-[36px] mb-[12px]"
-            />
-            <div className="flex flex-col max-w-[350px] mb-[20px] w-full">
-              <SignInForm />
+    <>
+      <main
+        className="flex flex-col bg-primary_background justify-center min-w-[100vh]"
+        role="main"
+      >
+        <article className="flex flex-row grow shrink-0 max-w-[--polaris-site-width-wide] w-full mt-8 pb-8 justify-center mx-auto align-top">
+          <div className="self-center pt-[27px] h-[581.15px] mb-3 bg-[length:468.32px_634.15px] bg-[-46px_0px] basis-[380.32px] mr-8 bg-[url(https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones-2x.png?__makehaste_cache_breaker=73SVAexZgBW)]">
+            <div className="relative h-[554.15px] w-[250px] ml-[112px]">
+              <ImageCarousel />
             </div>
           </div>
-          <div className="flex flex-col bg-primary_background border-[1px] border-elevated_separator items-center box-border py-[10px] mb-[10px]">
-            <p className="text-sm m-[15px]">
-              Don&apos;t have an account?&nbsp;
-              <a
-                href="/accounts/emailsignup/"
-                role="link"
-                className="touch-manipulation"
-              >
-                <span className="text-primary_button inline font-semibold">
-                  Sign up
+          <div className="flex flex-col grow shrink-0 justify-center mt-[12px] max-w-[350px]">
+            <div className="flex flex-col bg-primary_background border-[1px] border-elevated_separator items-center box-border mb-[10px] py-[10px] relative text-primary_text">
+              <button
+                type="button"
+                className="inline-block bg-[url('https://static.cdninstagram.com/rsrc.php/v3/ym/r/BQdTmxpRI6f.png')] bg-cover bg-no-repeat w-[175px] h-[51px] mt-[36px] mb-[12px]"
+              />
+              <div className="flex flex-col max-w-[350px] mb-[12px] w-full">
+                <SignInForm />
+              </div>
+            </div>
+            <div className="flex flex-col bg-primary_background border-[1px] border-elevated_separator items-center box-border py-[6px] mb-[10px]">
+              <p className="text-sm m-[15px] leading-[18px] font-sans">
+                Don&apos;t have an account?&nbsp;
+                <a
+                  href="/accounts/emailsignup/"
+                  role="link"
+                  className="touch-manipulation"
+                >
+                  <span className="text-primary_button inline font-semibold">
+                    Sign up
+                  </span>
+                </a>
+              </p>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="mx-5 my-[10px]">
+                <span className="text-sm leading-[18px] block font-sans before:block before:mt-[-3px] before:h-0 after:block after:mb-[-4px] after:h-0">
+                  Get the app.
                 </span>
-              </a>
-            </p>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="mx-5 my-[10px]">
-              <span className="text-sm leading-[18px] block font-sans before:block before:mt-[-3px] before:h-0 after:block after:mb-[-4px] after:h-0">
-                Get the app.
-              </span>
-            </div>
-            <div className="flex flex-row items-center my-[10px]">
-              <a
-                href="https://apps.apple.com/app/instagram/id389801252?ct=igweb.unifiedHome.badge&mt=8&pt=428156&vt=lo"
-                target="_blank"
-                aria-label="Download on the App Store"
-                className="mr-2"
-              >
-                <img
-                  src="https://static.cdninstagram.com/rsrc.php/v3/yt/r/Yfc020c87j0.png"
-                  alt="Download on the App Store"
-                  className="h-[40px]"
-                />
-              </a>
-              <a
-                href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3DE46822FE-BB37-48F6-9B2E-8F2E049FF9F8%26utm_campaign%3DunifiedHome%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge"
-                target="_blank"
-                aria-label="Get it on Google Play"
-              >
-                <img
-                  src="https://static.cdninstagram.com/rsrc.php/v3/yz/r/c5Rp7Ym-Klz.png"
-                  alt="Get it on Google Play"
-                  className="h-[40px]"
-                />
-              </a>
+              </div>
+              <div className="flex flex-row items-center my-[10px]">
+                <a
+                  href="https://apps.apple.com/app/instagram/id389801252?ct=igweb.unifiedHome.badge&mt=8&pt=428156&vt=lo"
+                  target="_blank"
+                  aria-label="Download on the App Store"
+                  className="mr-2"
+                >
+                  <img
+                    src="https://static.cdninstagram.com/rsrc.php/v3/yt/r/Yfc020c87j0.png"
+                    alt="Download on the App Store"
+                    className="h-[40px]"
+                  />
+                </a>
+                <a
+                  href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3DE46822FE-BB37-48F6-9B2E-8F2E049FF9F8%26utm_campaign%3DunifiedHome%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge"
+                  target="_blank"
+                  aria-label="Get it on Google Play"
+                >
+                  <img
+                    src="https://static.cdninstagram.com/rsrc.php/v3/yz/r/c5Rp7Ym-Klz.png"
+                    alt="Get it on Google Play"
+                    className="h-[40px]"
+                  />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </article>
-    </main>
+        </article>
+      </main>
+      <Footer />
+    </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,175 @@
+"use client";
+
+import React from "react";
+
+/**
+ * Represents a link in the footer navigation.
+ * @property displayName - The text to display for the link.
+ * @property href - The URL the link points to.
+ */
+interface FooterLink {
+  displayName: string;
+  href: string;
+}
+
+const footerLinks: FooterLink[] = [
+  { displayName: "Meta", href: "https://about.meta.com" },
+  { displayName: "About", href: "https://about.instagram.com/" },
+  { displayName: "Blog", href: "https://about.instagram.com/blog/" },
+  { displayName: "Jobs", href: "https://about.instagram.com/about-us/careers" },
+  { displayName: "Help", href: "https://help.instagram.com/" },
+  {
+    displayName: "API",
+    href: "https://developers.facebook.com/docs/instagram",
+  },
+  { displayName: "Privacy", href: "https://www.instagram.com/legal/privacy/" },
+  {
+    displayName: "Consumer Health Privacy",
+    href: "https://www.instagram.com/legal/privacy/health_privacy_policy/",
+  },
+  { displayName: "Terms", href: "https://www.instagram.com/legal/terms/" },
+  {
+    displayName: "Locations",
+    href: "https://www.instagram.com/explore/locations/",
+  },
+  {
+    displayName: "Instagram Lite",
+    href: "https://www.instagram.com/web/lite/",
+  },
+  { displayName: "Threads", href: "https://www.threads.net/" },
+  {
+    displayName: "Contact Uploading & Non-Users",
+    href: "https://www.facebook.com/help/instagram/261704639352628",
+  },
+  {
+    displayName: "Meta Verified",
+    href: "https://www.instagram.com/accounts/meta_verified/?entrypoint=web_footer",
+  },
+];
+
+/**
+ * Represents a language option in the language selection menu.
+ * @property id - The id of the language
+ * @property displayName - The text to display for the language.
+ */
+interface LanguageOption {
+  id: string;
+  displayName: string;
+}
+
+const languageOptions: LanguageOption[] = [
+  { id: "af", displayName: "Afrikaans" },
+  { id: "ar", displayName: "العربية" },
+  { id: "cs", displayName: "Čeština" },
+  { id: "da", displayName: "Dansk" },
+  { id: "de", displayName: "Deutsch" },
+  { id: "el", displayName: "Ελληνικά" },
+  { id: "en", displayName: "English" },
+  { id: "en-gb", displayName: "English (UK)" },
+  { id: "es", displayName: "Español (España)" },
+  { id: "es-la", displayName: "Español" },
+  { id: "fa", displayName: "فارسی" },
+  { id: "fi", displayName: "Suomi" },
+  { id: "fr", displayName: "Français" },
+  { id: "he", displayName: "עברית" },
+  { id: "id", displayName: "Bahasa Indonesia" },
+  { id: "it", displayName: "Italiano" },
+  { id: "ja", displayName: "日本語" },
+  { id: "ko", displayName: "한국어" },
+  { id: "ms", displayName: "Bahasa Melayu" },
+  { id: "nb", displayName: "Norsk" },
+  { id: "nl", displayName: "Nederlands" },
+  { id: "pl", displayName: "Polski" },
+  { id: "pt-br", displayName: "Português (Brasil)" },
+  { id: "pt", displayName: "Português (Portugal)" },
+  { id: "ru", displayName: "Русский" },
+  { id: "sv", displayName: "Svenska" },
+  { id: "th", displayName: "ภาษาไทย" },
+  { id: "tl", displayName: "Filipino" },
+  { id: "tr", displayName: "Türkçe" },
+  { id: "zh-cn", displayName: "中文(简体)" },
+  { id: "zh-tw", displayName: "中文(台灣)" },
+  { id: "bn", displayName: "বাংলা" },
+  { id: "gu", displayName: "ગુજરાતી" },
+  { id: "hi", displayName: "हिन्दी" },
+  { id: "hr", displayName: "Hrvatski" },
+  { id: "hu", displayName: "Magyar" },
+  { id: "kn", displayName: "ಕನ್ನಡ" },
+  { id: "ml", displayName: "മലയാളം" },
+  { id: "mr", displayName: "मराठी" },
+  { id: "ne", displayName: "नेपाली" },
+  { id: "pa", displayName: "ਪੰਜਾਬੀ" },
+  { id: "si", displayName: "සිංහල" },
+  { id: "sk", displayName: "Slovenčina" },
+  { id: "ta", displayName: "தமிழ்" },
+  { id: "te", displayName: "తెలుగు" },
+  { id: "ur", displayName: "اردو" },
+  { id: "vi", displayName: "Tiếng Việt" },
+  { id: "zh-hk", displayName: "中文(香港)" },
+  { id: "bg", displayName: "Български" },
+  { id: "fr-ca", displayName: "Français (Canada)" },
+  { id: "ro", displayName: "Română" },
+  { id: "sr", displayName: "Српски" },
+  { id: "uk", displayName: "Українська" },
+];
+
+const Footer = () => {
+  // TODO: Temporarily setting language here. Replace after internalization.
+  const currLang = "en";
+
+  const changeLanguage = () => {};
+
+  return (
+    <footer
+      role="contentinfo"
+      className="pb-[52px] pt-6 flex flex-col gap-[6px]"
+    >
+      <ul className="flex justify-center gap-4">
+        {footerLinks.map((link, i) => (
+          <li key={i}>
+            <a href={link.href || "#"}>
+              <span className="text-xs font-sans text-secondary_text">
+                {link.displayName || "Untitled"}
+              </span>
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="flex gap-[17px] justify-center mt-[13px] mb-[9px] text-secondary_text text-xs font-sans leading-[18px] overflow-y-visible">
+        <div className="flex relative">
+          <div className="flex">
+            <span className="">English</span>
+            <div className="transform rotate-180 relative ml-[5px] top-[-3px]">
+              <svg
+                aria-label="Down chevron icon"
+                role="img"
+                viewBox="0 0 24 24"
+                className="fill-current w-3 h-3"
+              >
+                <title>Down chevron icon</title>
+                <path d="M21 17.502a.997.997 0 0 1-.707-.293L12 8.913l-8.293 8.296a1 1 0 1 1-1.414-1.414l9-9.004a1.03 1.03 0 0 1 1.414 0l9 9.004A1 1 0 0 1 21 17.502Z"></path>
+              </svg>
+            </div>
+          </div>
+          <select
+            className="bg-transparent absolute w-full top-[1px] opacity-0"
+            value={currLang}
+            aria-label="Switch Display Language"
+            onChange={() => {
+              return;
+            }}
+          >
+            {languageOptions.map((lang) => (
+              <option key={lang.id} value={lang.id}>
+                {lang.displayName}
+              </option>
+            ))}
+          </select>
+        </div>
+        <span className="">&#169; 2024 Instagram from Meta</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
